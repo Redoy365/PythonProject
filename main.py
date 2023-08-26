59fbaf5d22d7bada1a29ee6359cdd42acd72d103
Redoy365 <mdresponsivejb@gmail.com>
2023-08-26 21:24:42 +0600

    v1.1.2

--- main.py
+++ main.py
@@ -13,19 +13,25 @@ class Hospital:
         self.root.geometry("1600x800+0+0")
         # =======================   Header  ===========================
         lbltitle = Label(self.root, bd=20, relief=RIDGE,text="HOSPITAL MANAGEMENT SYSTEM", fg="red", bg="white", font=("times new roman",50,"bold"))
-        lbltitle.pack(side=TOP, fill=X)
+        lbltitle.place(relx=0, rely=0, relheight=0.15, relwidth=1)
         # =======================    Dataframe   =======================
         Dataframe = Frame(self.root, bd=20, relief=RIDGE,height=400)
-        Dataframe.pack(side=TOP, fill=X)
+        Dataframe.place(relx=0, rely=0.15, relheight=0.5, relwidth=1)
 
         DataframeLeft = LabelFrame(Dataframe, bd=10, relief=RIDGE, padx=10,font=("arial",12,"bold"), text="Patient Information")
-        DataframeLeft.place(relwidth=0.6,height=350)
+        DataframeLeft.place(relx=0, rely=0, relheight=1, relwidth=0.6)
 
         # separator = ttk.Separator(root, orient='vertical')
         # separator.place(relx=0.61, rely=0.2, relwidth=0.009, relheight=0.4)
 
         DataframeLeft = LabelFrame(Dataframe, bd=10, relief=RIDGE, padx=10,font=("arial",12,"bold"), text="Patient Information")
-        DataframeLeft.place(relx=0.6,rely=0,relwidth=0.4,height=350)
+        DataframeLeft.place(relx=0.6, rely=0, relheight=1, relwidth=0.4)
+        # ===========================   Button  =========================
+        Dataframe = Frame(self.root, bd=20, relief=RIDGE)
+        Dataframe.place(relx=0, rely=0.65, relheight=0.1, relwidth=1)
+        # ===========================   DataBase    =====================
+        Dataframe = Frame(self.root, bd=20, relief=RIDGE)
+        Dataframe.place(relx=0, rely=0.75, relheight=0.25, relwidth=1)
 
 root = Tk()
 ob = Hospital(root)
